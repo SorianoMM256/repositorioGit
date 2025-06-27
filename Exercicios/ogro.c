e3f0db0952e01cc45fbbbb8ca56de0b6c214f0ae
Soriano <matheusmottasoriano@gmail.com>
2025-06-27 09:56:34 -0300

    Problemas Resolvidos

---
+++ Exercicios/ogro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int tam, aux, total = 0;
+    scanf("%d", &tam);
+    int vet[tam];
+    for(int i = 0; i < tam, i++){
+        scanf("%d", &aux);
+        total += aux;
+    }
+    printf("%d\n", total);
+    return 0;
+}
