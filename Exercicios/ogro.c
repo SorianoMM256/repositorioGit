1590d8f3c5a12037e57efcaa8e93bd604cc61ecb
Soriano <matheusmottasoriano@gmail.com>
2025-06-27 09:59:15 -0300

    Teste

--- Exercicios/ogro.c
+++ Exercicios/ogro.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    int tam, aux, total = 0;
+int main()
+{
+    int tam, temp, total = 0;
     scanf("%d", &tam);
     int vet[tam];
-    for(int i = 0; i < tam, i++){
-        scanf("%d", &aux);
-        total += aux;
+    for (int i = 0; i < tam, i++)
+    {
+        scanf("%d", &temp);
+        total += temp;
     }
     printf("%d\n", total);
     return 0;
